233d5bdbfadb681f1394442e91fa9a2f8ff8e822
halliwell2046 <halliwell2046@yahoo.com>
2020-07-17 21:47:48 -0400

    final submit after vsc issues

--- index.js
+++ index.js
@@ -199,8 +199,15 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor (object) {
+    super(object)
+    this.gradClassName = object.gradClassName,
+    this.favInstructor = object.favInstructor
+  }
+  standUp(channel){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 }
 
 /*
